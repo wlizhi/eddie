9bd9a0896b1bc9f8d517479dbbc53ed71696423d
eddie <2456767505@qq.com>
2026-06-22 18:24:08 +0800

    tomcat线程池参数调优，本地镜像构建参数调优。

--- build-native.sh
+++ build-native.sh
@@ -5,4 +5,4 @@ sh "$SCRIPT_DIR/frontend/build.sh"
 cd "$SCRIPT_DIR" || exit
 mvn clean
 mvn install -Pnative -pl ai-app -am -DskipTests
-mvn -Pnative native:compile -pl ai-app -DskipTests
+mvn -Pnative native:compile -pl ai-app -DskipTests -Dnative-image.buildArgs="-J-Xmx8g"
