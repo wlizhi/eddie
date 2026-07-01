b0ed66526e4a4caf300fb8034fa28a4a0f1f7a22
eddie <2456767505@qq.com>
2026-07-01 10:48:56 +0800

    chore: 移除 build-native.sh

--- build-native.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
-sh "$SCRIPT_DIR/frontend/build.sh"
-
-cd "$SCRIPT_DIR" || exit
-mvn clean
-mvn install -Pnative -pl ai-app -am -DskipTests
-mvn -Pnative native:compile -pl ai-app -DskipTests -Dnative-image.buildArgs="-J-Xmx10g"
